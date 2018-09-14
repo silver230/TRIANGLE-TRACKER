defae00b12128d941b4c47a335996ba74b47474a
silver230 <sylvesteromondi49@gmail.com>
2018-09-14 16:01:13 +0300

    added variable function for line CA

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 function triangle(){
     var A = parseFloat(document.getElementById('AB').value);
     var B = parseFloat(document.getElementById('BC').value);
+    var C = parseFloat(document.getElementById('CA').value);
