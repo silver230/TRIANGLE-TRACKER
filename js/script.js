2056c5265fb4c752bb10324fd9dab0714ffb8b66
silver230 <sylvesteromondi49@gmail.com>
2018-09-14 16:02:23 +0300

    added variable defination of line AB function

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ function triangle(){
     var A = parseFloat(document.getElementById('AB').value);
     var B = parseFloat(document.getElementById('BC').value);
     var C = parseFloat(document.getElementById('CA').value);
+
+    var AB = A + B;
